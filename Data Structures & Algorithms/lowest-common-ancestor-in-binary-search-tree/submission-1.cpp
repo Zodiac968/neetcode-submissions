4e8c2ba2991fd90733225d6179c34d20cb412afa
Zodiac968 <60397316+Zodiac968@users.noreply.github.com>
2026-06-30 10:04:58 +0530

    Add: lowest-common-ancestor-in-binary-search-tree - submission-1

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-1.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<TreeNode*> findPath(TreeNode* root, TreeNode *p, vector<TreeNode*> v){
+        if(root == nullptr) {
+            v.clear();
+            return v;
+        }
+        v.push_back(root);
+        if(root == p) {
+            return v;
+        }
+        vector<TreeNode*> left = findPath(root->left, p, v);
+        vector<TreeNode*> right = findPath(root->right, p, v);
+        if(!left.empty()) return left;
+        else if(!right.empty()) return right;
+        v.clear();
+        return v;
+    }
+
+    void printPath(vector<TreeNode*> p){
+        for(int i = 0; i < p.size(); i++){
+            cout << p[i]->val << " -> ";
+        }
+        cout << endl;
+    }
+
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        vector<TreeNode*> pp = findPath(root, p, {});
+        vector<TreeNode*> qq = findPath(root, q, {});
+        int i;
+        if(pp.size() < qq.size()) i = pp.size()-1;
+        else i = qq.size()-1;
+        while(i >= 0){
+            if(pp[i] == qq[i]) return pp[i];
+            else i--;
+        }
+        return root;
+    }
+};
